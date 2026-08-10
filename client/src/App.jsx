c0c80a822d9b508c7fb492cf9037edec475a0939
Sakshi Srivastava <sakshisrivas33@gmail.com>
2026-08-10 13:52:06 +0530

    Fix ServerUrl to correct domain

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import InterviewHistory from './pages/InterviewHistory.jsx'
 import Pricing from './pages/Pricing.jsx'
 import InterviewReport from './pages/InterviewReport.jsx'
 
-export const ServerUrl = "https://interviewagent-ai-server.onrender.com"
+export const ServerUrl = "https://interviewagent-aiserver.onrender.com"
 
 function App() {
 
